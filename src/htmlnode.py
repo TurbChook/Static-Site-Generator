cc916b5d2f14fc4f9c62dbd2f1d21112c29a0ebc
TurbChook <john.criollo.a@gmail.com>
2026-02-06 00:28:31 +0100

    Adding and testing a ParentNode subclass

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,3 +35,17 @@ class LeafNode(HTMLNode):
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag,None, children, props)
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        array = ''
+        for child in self.children:
+            array += child.to_html()
+        return f"<{self.tag}>{array}</{self.tag}>"
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
