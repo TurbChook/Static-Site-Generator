75533684a6a0483138b138763c4c9a5ec17a9c95
TurbChook <john.criollo.a@gmail.com>
2026-02-07 15:27:28 +0100

    Adding split_nodes_delimiter

--- src/textnode.py
+++ src/textnode.py
@@ -35,6 +35,23 @@ def text_node_to_html_node(text_node):
             return LeafNode('img','',{'src':text_node.url,'alt':text_node.text})
         case _:
             raise ValueError('Invalid TextType')
-
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            result.append(node)
+            continue
+        split_nodes = []
+        sections = node.text.split(delimiter)
+        if len(sections)%2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
         
-    
+        for i in range(len(sections)):
+            if sections[i] == '':
+                continue
+            if i%2 == 0:
+                split_nodes.append(TextNode(sections[i],TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i],text_type))
+        result.extend(split_nodes)
+    return result
